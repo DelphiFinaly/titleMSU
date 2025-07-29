2959c7afa2bfbbd27bb2a319ecc3ab8df0126067
DelphiFinaly <klresteduard@gmail.com>
2025-07-29 09:55:07 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/titleMSU/', // ← имя репозитория на GitHub
+  base: 'titleMSU', // ← имя репозитория на GitHub
   plugins: [react()],
 })
